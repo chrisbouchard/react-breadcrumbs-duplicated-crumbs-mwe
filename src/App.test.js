9348c974f9c4d519cd8f4ef9d56af3a3da9bc9a0
Chris Bouchard <chris@upliftinglemma.net>
2021-07-02 21:33:30 -0400

    Add failing unit test
    This unit test should *not* be failing, but for some reason Jest+jsdom
    doesn't give the same behavior as either Firefox or Chrome. One thought
    is it may be timing related.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, waitFor } from "@testing-library/react";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("the breadcrumbs contain crumb twice", async () => {
+  const { getAllByText } = render(<App />);
+  await waitFor(() => expect(getAllByText("Crumb")).toHaveLength(2));
 });
